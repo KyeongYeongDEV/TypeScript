ecbb50a55d1a4e939d9d697c98866423eb1a5e1a
KyeongYeongDEV <yeong7098@naver.com>
2024-03-10 23:13:49 +0900

    mysql connect

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import { UserDto } from "./dto/user-dto";
 import { UserRequestDto } from "./dto/request/user-dto.request";
 import apiIndex from "./api/index"
+import { connection } from "./config/db";
 
 const app: Application = express();
 
@@ -11,6 +12,14 @@ const port: number = 3000;
 app.use(express.json());
 //json형태 파일 디코딩 미들웨어
 app.use(express.urlencoded({ extended: false }));
+
+connection.connect((err)=>{
+    if(err){
+        console.log("fall");
+    }else{
+        console.log("succsses");
+    }
+})
 
 app.use('/api', apiIndex);
 
@@ -30,4 +39,16 @@ app.use('/api', apiIndex);
 //app.use(customErrorMiddleware);
 app.listen(port, () => {
     console.log(`App is listening on port ${port} !`);
+
+    const sql = "select *from users";
+    connection.query(sql,(error, results, fields) => {
+        if (error) {
+            console.error('Error executing query: ', error);
+            return;
+        } else {
+            for (const row of results as any) {
+                console.log(row.username);
+            }
+        }
+    });
 });
